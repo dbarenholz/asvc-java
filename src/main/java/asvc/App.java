f72bffc019b3b7742a70d01df7dbb80a5a8ffd66
MrMeNr1 <d.barenholz@student.tue.nl>
2020-01-07 22:29:26 +0100

    Initial setup

---
+++ src/main/java/asvc/App.java
@@ -0,0 +1,16 @@
+package asvc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Test.");
+    }
+}
