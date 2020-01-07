854a1ae1462c3faff0735ff3a8216ba956505777
Daniel Barenholz <d.barenholz@student.tue.nl>
2020-01-07 22:36:57 +0100

    Initial commit Create project setup

--- src/main/java/asvc/App.java
+++ src/main/java/asvc/App.java
@@ -3,6 +3,9 @@ package asvc;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ *
+ */
 public class App extends Application {
 
     public static void main(String[] args) {
