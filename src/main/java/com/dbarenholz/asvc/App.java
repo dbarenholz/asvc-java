59b152564075a1670180a5a18c8668b2c576dc04
MrMeNr1 <d.barenholz@student.tue.nl>
2020-01-10 20:18:02 +0100

    Create methods for building GUI

--- src/main/java/com/dbarenholz/asvc/App.java
+++ src/main/java/com/dbarenholz/asvc/App.java
@@ -1,29 +1,152 @@
 package com.dbarenholz.asvc;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * ASVC Application class.
+ *
+ * TODO: Write class documentation.
+ * TODO: Write package.java documentation.
  */
 public class App extends Application {
 
     // === Variables === //
     private static final Logger logger = LogManager.getLogger();
 
-    // === Helper methods === //
+    // === Helpers === //
     /**
      * Checks if there is a .ini file present for user settings.
      *
-     * @return {@code false} if not present, {@code true} otherwise.
+     * @return {@code true} if present, readable, and writeable. Return {@code false} otherwise.
      */
     private boolean settingsIniFilePresent() {
-        // TODO: Write settingsIniFilePresent method
-        return false;
+        logger.debug("Checking {} for ini settings", Settings.iniPath);
+        File tempFileObject = new File(Settings.iniPath);
+        // Check for existence, reading and writing
+        if (!tempFileObject.exists()) {
+            return false;
+        } else if (!tempFileObject.canRead()) {
+            logger.debug("File exists at previously logged path, but application cannot read.");
+            return false;
+        } else if (!tempFileObject.canWrite()){
+            logger.debug("File exists at previously logged path, but application cannot write.");
+            return false;
+        }
+        return true;
+    }
+
+    private Tab mainTab() {
+        Tab tab = new Tab("ASVC");
+        GridPane container = new GridPane();
+        Label temp = new Label("ASVC Tab");
+
+        container.add(temp, 0, 0);
+
+        tab.setContent(container);
+        return tab;
+    }
+
+    /**
+     * Creates the about tab for the ASVC application.
+     *
+     * @return about tab
+     */
+    private Tab aboutTab() {
+        Tab tab = new Tab("About");
+
+        GridPane container = new GridPane();
+        Label temp = new Label("About Tab");
+
+        container.add(temp, 0, 0);
+
+        tab.setContent(container);
+        return tab;
+    }
+
+    /**
+     * Creates the settings tab for the ASVC application
+     *
+     * @return settings tab
+     */
+    private Tab settingsTab() {
+        Tab tab = new Tab("Settings");
+
+        GridPane container = new GridPane();
+        Button updateButton = new Button("Reload from .ini");
+        Button saveButton = new Button("Save to .ini");
+
+        updateButton.setOnAction(e -> {
+            if (!Settings.readFromIni()) {
+                logger.warn("Could not reload settings from ini.");
+            }
+        });
+
+        saveButton.setOnAction(e -> {
+            if (!Settings.writeToIni()) {
+                logger.warn("Could not write settings to ini.");
+            }
+        });
+
+        container.add(updateButton, 0, 0);
+        container.add(saveButton, 1, 0);
+
+        tab.setContent(container);
+        return tab;
+    }
+
+    /**
+     * Creates tab pane.
+     *
+     * @return tab pane
+     */
+    private TabPane buildTabPane() {
+        TabPane tabPane = new TabPane();
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        tabPane.setTabDragPolicy(TabPane.TabDragPolicy.FIXED);
+        return tabPane;
+    }
+
+    // === Methods === //
+    /**
+     * Helper method to build the GUI.
+     */
+    private void createGUI(Stage stage) {
+        logger.info("Build application GUI...");
+
+        // Create list of all tabs of application
+        List<Tab> tabs = new ArrayList<>();
+        tabs.add(mainTab());
+        tabs.add(settingsTab());
+        tabs.add(aboutTab());
+
+        // Add all created tabs to the root
+        TabPane rootTabPane = buildTabPane();
+        rootTabPane.getTabs().addAll(tabs);
+
+        // Add root to the scene, and set the scene
+        Scene scene = new Scene(rootTabPane, Settings.prefWidth, Settings.prefHeight);
+        stage.setMinWidth(Settings.minWidth);
+        stage.setMinHeight(Settings.minHeight);
+        stage.setScene(scene);
+        stage.setTitle(Settings.applicationTitle);
+        stage.show();
     }
 
     /**
@@ -34,35 +157,16 @@ public class App extends Application {
         // Check if .ini file present
         if (!settingsIniFilePresent()) {
             // if not present -- set defaults and save to .ini
-            logger.debug("Settings .ini file not present. Creating one...");
+            logger.debug("Settings .ini file cannot be used. Creating one...");
+            if (!Settings.writeToIni()) {
+                logger.error("Writing settings to ini failed. Closing application...");
+            }
         }
         // .ini is present (newly created, or was already present)
-        logger.debug("Settings .ini file present. Setting settings from file...");
-    }
-
-    /**
-     * Helper method to build the GUI.
-     */
-    private void createGUI(Stage stage) {
-        logger.info("Build application GUI...");
-
-        // TODO: Build entire GUI.
-        Label label = new Label("Hello World, JavaFX !");
-        Scene scene = new Scene(label, 400, 200);
-        stage.setScene(scene);
-        stage.setTitle("Temp title");
-        stage.show();
-    }
-
-    // === Main methods === //
-    /**
-     * Java's {@code main} method. Used to launch application.
-     *
-     * @param arguments command-line arguments (not used)
-     */
-    public static void main(String[] arguments) {
-        logger.info("Starting application...");
-        launch(arguments);
+        logger.debug("Settings .ini can be used. Setting settings from file...");
+        if (!Settings.readFromIni()) {
+            logger.error("Reading settings from ini failed. Closing application...");
+        }
     }
 
     /**
@@ -75,5 +179,29 @@ public class App extends Application {
     public void start(Stage applicationStage) {
         initialiseSettings();
         createGUI(applicationStage);
+        // Log closing of application
+        applicationStage.setOnCloseRequest(event -> {
+            logger.info("Closing application...");
+            System.exit(0);
+        });
+
+    }
+
+    /**
+     * Java's {@code main} method. Used to launch application.
+     *
+     * @param arguments command-line arguments (not used)
+     */
+    public static void main(String[] arguments) {
+        logger.info("Starting application...");
+        try {
+            launch(arguments);
+        } catch (Exception exception) {
+            logger.fatal("Application crash.");
+            logger.fatal("Error class:\n{}", exception.getClass());
+            logger.fatal("Error cause:\n{}", (Object) exception.getCause());
+            logger.fatal("Error message:\n{}", exception.getMessage());
+            logger.fatal("Error stacktrace:\n{}", (Object) exception.getStackTrace());
+        }
     }
 }
--- src/main/java/com/dbarenholz/asvc/Settings.java
+++ src/main/java/com/dbarenholz/asvc/Settings.java
@@ -1,10 +1,33 @@
 package com.dbarenholz.asvc;
 
+import javafx.geometry.Dimension2D;
+import javafx.util.Pair;
+
 /**
  * Settings class.
  *
  * Contains all (global, immutable) application settings.
  */
 public class Settings {
+    // === Non editable settings === //
+    // TODO: Set default application title
+    final static String applicationTitle = "ASVC";
+    final static double minWidth = 250.0;
+    final static double minHeight = 250.0;
 
+    // === User editable settings for ini === //
+    final static double prefWidth = 500.0;
+    final static double prefHeight = 500.0;
+
+    // TODO: Set default path
+    final static String iniPath = "path/to/file.ini";
+
+    // TODO: Implement method to write all settings to ini file
+    static boolean writeToIni() {
+        return false;
+    }
+
+    static boolean readFromIni() {
+        return false;
+    }
 }
